6091797268bf7601c70bbf1d3ef5736a1865532e
Donatas Ramanauskas <donatasramanauskas1@gmail.com>
2019-07-20 16:12:15 +0300

    Added migrations for device app

--- backend/control_plane/devices/models.py
+++ backend/control_plane/devices/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Device(models.Model):
     name = models.CharField(max_length=20)
     type = models.CharField(max_length=20)
-    ip = models.IPAddressField()
+    ip = models.GenericIPAddressField()
