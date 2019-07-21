f8907dccf429e9d469432db1822b2b727eeb8e49
Donatas Ramanauskas <donatasramanauskas1@gmail.com>
2019-07-21 16:04:55 +0300

    Added unittests for LisDevices view

--- backend/control_plane/devices/tests.py
+++ backend/control_plane/devices/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+from .models import Device
+
+
+class ListDevicesViewTests(TestCase):
+
+    def setUp(self):
+        self.device1 = Device.objects.create(
+            name='device1',
+            type='type1',
+            ip='10.0.0.1'
+        )
+        self.device2 = Device.objects.create(
+            name='device2',
+            type='type2',
+            ip='10.0.0.2'
+        )
+
+    def test_list_devices(self):
+        url = reverse('devices:list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data[0]['name'], 'device1')
+        self.assertEqual(response.data[0]['type'], 'type1')
+        self.assertEqual(response.data[1]['name'], 'device2')
+        self.assertEqual(response.data[1]['type'], 'type2')
