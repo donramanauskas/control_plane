76d4f3d48184a1c7af7f4689a19f8aaf3963c9ba
Donatas Ramanauskas <donatasramanauskas1@gmail.com>
2019-07-21 15:04:00 +0300

    Added ListAllDevices view

--- backend/control_plane/devices/views.py
+++ backend/control_plane/devices/views.py
@@ -1,7 +1,12 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 
 from .models import Device
 from .serializers import DeviceSerializer
+
+
+class ListAllDevices(ListAPIView):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
 
 
 class AddNewDevice(CreateAPIView):
