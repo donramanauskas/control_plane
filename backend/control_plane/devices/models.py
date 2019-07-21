62ae38699c98f5e7bc55c7f4cf8c1f416fb1b8b7
Donatas Ramanauskas <donatasramanauskas1@gmail.com>
2019-07-21 18:33:47 +0300

    Added ongoing_maintenance field

--- backend/control_plane/devices/models.py
+++ backend/control_plane/devices/models.py
@@ -5,3 +5,4 @@ class Device(models.Model):
     name = models.CharField(max_length=20)
     type = models.CharField(max_length=20)
     ip = models.GenericIPAddressField()
+    ongoing_maintenance = models.BooleanField(default=False)
