47d8c181a683a4d8eb3283431dac546bbf8cbdf8
Donatas Ramanauskas <donatasramanauskas1@gmail.com>
2019-07-21 20:21:28 +0300

    Added view that updates device maintenance status

--- backend/control_plane/devices/urls.py
+++ backend/control_plane/devices/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.ListAllDevices.as_view(), name='list'),
     path('on_maintenance', views.DevicesOnMaintenance.as_view(), name='on_maintenance'),
-    path('update_maintenance_status/<int:id>', views.UpdateDeviceMaintenanceStatus.as_view(), name='update_maintenance_status'),
+    path('update_maintenance_status/<int:id>',
+         views.UpdateDeviceMaintenanceStatus.as_view(), name='update_maintenance_status'),
     path('add', views.AddNewDevice.as_view(), name='add'),
     path('delete/<int:id>', views.DeleteDevice.as_view(), name='delete'),
 ]
