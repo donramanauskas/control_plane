2d8dabbcfae3f21bad03a436984bca07d8067b4f
Donatas Ramanauskas <donatasramanauskas1@gmail.com>
2019-07-21 15:24:39 +0300

    Added url for listing all devices

--- backend/control_plane/devices/urls.py
+++ backend/control_plane/devices/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.ListAllDevices.as_view(), name='list'),
     path('add', views.AddNewDevice.as_view(), name='add')
 ]
