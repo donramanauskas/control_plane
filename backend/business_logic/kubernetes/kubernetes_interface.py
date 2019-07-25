a567b5ac87a0fb8786bdc10eea0aaaadc4de156d
Donatas Ramanauskas <donatasramanauskas1@gmail.com>
2019-07-25 18:48:46 +0300

    Added tests for KubernetesPodInfoView

--- backend/business_logic/kubernetes/kubernetes_interface.py
+++ backend/business_logic/kubernetes/kubernetes_interface.py
@@ -3,11 +3,9 @@ from kubernetes import client, config
 
 class KubernetesInterface:
 
-    def __init__(self):
+    def info(self):
         config.load_kube_config()
         self.v1 = client.CoreV1Api()
-
-    def info(self):
         ret = self.v1.list_pod_for_all_namespaces(watch=False)
         pods = []
         for i in ret.items:
--- backend/control_plane/kubernetes_manager/tests.py
+++ backend/control_plane/kubernetes_manager/tests.py
@@ -1,10 +1,27 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from unittest.mock import patch
 
-class KubernetesManagerInfo(TestCase):
+
+class KubernetesManagerInfoViewTests(TestCase):
 
     def test_kubernetes_manager_info_view(self):
         url = reverse('kubernetes_manager:info')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+
+class KubernetesPodsInfoViewTests(TestCase):
+
+    @patch('business_logic.kubernetes.kubernetes_interface.KubernetesInterface.info',
+           return_value=[{'172.17.0.3': {'kube-system': 'coredns-5c98db65d4-2s58n'}}])
+    def test_kubernetes_pod_info_view(self, mock_kubernetes_api_call):
+        """
+        Test get Kubernetes POD info view.
+        API call to external Kubernetes api mocked by mock_kubernetes_api_call
+        """
+
+        url = reverse('kubernetes_manager:pod_info')
+        response = self.client.get(url)
+        print(response.data)
