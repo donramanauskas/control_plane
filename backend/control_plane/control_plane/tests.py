1ff5c7dfc7c464456b80d65cd2f0e587282d5620
Donatas Ramanauskas <donatasramanauskas1@gmail.com>
2019-07-28 16:24:14 +0300

    Added add_zabbix_host method

--- backend/control_plane/control_plane/tests.py
+++ backend/control_plane/control_plane/tests.py
@@ -5,9 +5,4 @@ from business_logic.zabbix import ZabbixInterface
 class TestZabbixInterface(TestCase):
 
     def test_zabbix_info(self):
-        self.zabbix_instance = ZabbixInterface(
-            zabbix_data={
-                'ip': '10.1.1.1',
-            }
-        )
-        self.assertTrue(True)
+        pass
