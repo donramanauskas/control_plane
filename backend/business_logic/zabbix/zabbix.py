1fda52ac943fc6469a33efbc70df8c10e84912ef
Donatas Ramanauskas <donatasramanauskas1@gmail.com>
2019-07-28 16:23:23 +0300

    Added add_zabbix_host method

--- backend/business_logic/zabbix/zabbix.py
+++ backend/business_logic/zabbix/zabbix.py
@@ -1,15 +1,33 @@
-from pyzabbix.api import ZabbixAPI
+from pyzabbix import ZabbixAPI, ZabbixAPIException
 
 
 class ZabbixInterface:
 
     def __init__(self, zabbix_data):
         self.ip = zabbix_data['ip']
+        self.username = 'Admin'
+        self.password = 'zabbix'
+        self.zapi = ZabbixAPI("http://"+ self.ip + "/zabbix/")
+        self.zapi.login(self.username, self.password)
 
     def info(self):
         return self.ip
 
     def get_all_monitored_hosts(self):
-        zapi = ZabbixAPI(url="http://"+ self.ip + "/zabbix/", user='Admin', password='zabbix')
-        result1 = zapi.host.get(monitored_hosts=1, output='extend')
+
+        result1 = self.zapi.host.get(output='extend')
         return result1
+
+    def add_zabbix_host(self, host_id, name, key_, type, value_type):
+        try:
+            item = self.zapi.item.create(
+                host_id=host_id,
+                name=name,
+                key_=key_,
+                type=type,
+                value_type=value_type
+            )
+        except ZabbixAPIException as e:
+            print(e)
+
+
